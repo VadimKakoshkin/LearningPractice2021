f57dca4708b3ba2b868392edbbdc65a25431bd10
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2021-12-22 16:50:12 +0300

    added index.html, main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Переменные
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+//Логгеры
+alert("Hope has descended!");
+console.log("Console testing — Complete! =)");
