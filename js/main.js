30a129a431c5e3b7dd1c1ab6eae7c0b700e92fa9
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2021-12-27 11:55:28 +0300

    updated main.js

--- js/main.js
+++ js/main.js
@@ -5,17 +5,32 @@ let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-let rollback = 10;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 
 //Функции
+let getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+}
+
 const showTypeOf = function(variable) {
     console.log(variable, typeof variable);
+}
+
+const getFullPrice = function () {
+    return screenPrice + allServicePrices;
+}
+
+const getServicePercentPrice = function () {
+    return fullPrice - (fullPrice * (rollback / 100));
 }
 
 const getRollbackMessage = function() {
@@ -30,42 +45,27 @@ const getRollbackMessage = function() {
     }
 }
 
-let getAllServicePrices = function() {
-    return servicePrice1 + servicePrice2;
-}
-
-function getFullPrice() {
-    return screenPrice + getAllServicePrices();
-}
-
 const getTitle = function() {
-    for (let i = 0; i <= title.length - 1; i++) {
-        if (i == 0) {
-            title.charAt(i).split(" ");
-            title.charAt(i).toUpperCase();
-        } else {
-            title.charAt(i).toLowerCase();
-        }
-    }
-    return title;
-}
-
-const getServicePercentPrice = function () {
-    return fullPrice - (fullPrice * (rollback / 100));
+    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
 }
 
 //Вызов функций
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrice();
+title = getTitle();
+
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
 //Логгеры
-console.log(title);
-console.log(screens);
-
-console.log(getAllServicePrices());
-console.log(getFullPrice());
 console.log(getRollbackMessage(fullPrice));
-console.log(getServicePercentPrice());
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
 
-console.log("Стоимость вёрстки экранов " + screenPrice + " рублей/долларов/гривен/юаней и стоимость разработки сайта " + getFullPrice() + " рублей/долларов/гривен/юаней");
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log("Стоимость вёрстки экранов " + screenPrice + " юаней и стоимость разработки сайта " + fullPrice + " юаней");
