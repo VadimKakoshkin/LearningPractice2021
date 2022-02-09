2394f9c6479cd2b813a7f9eb350f2e40fe29b005
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-02-09 12:40:59 +0300

    Completed lesson15

--- js/main.js
+++ js/main.js
@@ -1,11 +1,59 @@
 //Переменные
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+"use strict";
+const body = document.querySelector("body");
+let elem;
+let p = prompt("Введите текст!");
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height + "px";
+  this.width = width + "px";
+  this.bg = bg;
+  this.fontSize = fontSize + "px";
+};
+
+//Функции
+DomElement.prototype.creater = function () {
+  if (this.selector[0] === ".") {
+    elem = document.createElement("div");
+    elem.classList.add(this.selector.slice(1));
+    elem.style.display = "flex";
+    elem.style.justifyContent = "center";
+    elem.style.alignItems = "center";
+    elem.style.margin = "30px";
+    elem.style.textTransform = "uppercase";
+  } else if (this.selector[0] === "#") {
+    elem = document.createElement("p");
+    elem.id = this.selector.slice(1);
+    elem.style.display = "flex";
+    elem.style.justifyContent = "center";
+    elem.style.alignItems = "center";
+    elem.style.margin = "30px";
+    elem.style.textTransform = "lowercase";
+  }
+
+  elem.style.height = this.height;
+  elem.style.width = this.width;
+  elem.style.background = this.bg;
+  elem.style.fontSize = this.fontSize;
+
+  body.append(elem);
+};
+
+DomElement.prototype.addText = function () {
+  elem.textContent = p;
+};
+
+//Элементы и их создание
+const elem1 = new DomElement(".block", 60, 200, "red", 30);
+const elem2 = new DomElement("#block", 30, 200, "green", 20);
+
+elem1.creater();
+elem1.addText();
+elem2.creater();
+elem2.addText();
 
 //Логгеры
-alert("Hope has descended!");
-console.log("Console testing — Complete! =)");
+console.log(elem1);
+console.log(elem2);
+console.dir(elem);
