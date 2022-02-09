3cc213cf731ddbaa04f70ada9306c64f1d63b8cd
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-02-09 08:50:47 +0300

    Complete hard02

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 //Переменные
-let title = "GitHub";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 33;
-let rollback = 74;
-let fullPrice = 1000000;
-let adaptive = true;
+let num = 266219;
+
+
+//Функции
+let numMultiply = (num) => {
+    if (!num)
+        return 0;
+    let sum = 1;
+    while (num) {
+        sum *= num % 10;
+        num = Math.floor(num / 10);
+    }
+    return sum;
+};
 
 //Логгеры
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов — " + screenPrice + " рублей/долларов/гривен/юани");
-console.log("Стоимость разработки сайта — " + fullPrice + " рублей/долларов/гривен/юани");
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100))
+console.log(numMultiply(num));
+num = numMultiply(num);
+num **= 3;
+console.log(num);
+console.log(String(num).slice(0,2));
