93a7a602d9b93cdefdf1a8f39d1bc711442a687c
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-02-09 09:46:53 +0300

    Complete hard04

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-//Переменные
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+'use strict';
+
+//Функция
+const max = (arg) => {
+    let res = 'Необходимо передать строку...';
+    if (typeof arg === "string") {
+        const maxLength = 30;
+        const str = arg.trim();
+        if (str.length > maxLength) {
+            res = str.substr(0, maxLength) + '...';
+        } else {
+            res = str;
+        }
+    }
+    return res;
+};
 
 //Логгеры
-alert("Hope has descended!");
-console.log("Console testing — Complete! =)");
+console.log('max(): ', max(5));
+console.log('max(): ', max('Строка менее 30 символов'));
+console.log('max(): ', max('Строка более 30 символов — Bidididididididididididididididididididididididididididi! Move it all!'));
