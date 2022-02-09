6127be250e8521210784f0ac28ea11d7da40efda
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-02-09 09:57:39 +0300

    Completed hard05

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-//Переменные
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+'use strict';
 
-//Логгеры
-alert("Hope has descended!");
-console.log("Console testing — Complete! =)");
+//Переменные
+let arr = [216,784,924,716,165,354,405]
+
+console.log('1)');
+
+//Проверка на числа, начинающиеся с 2 или 4
+arr.forEach(em => {
+  if (String(em).search(/(2|4)/) === 0) console.log(em);
+})
+
+console.log('');
+console.log('2)');
+
+//Проверка на простые числа
+function isSimple(n) {
+  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
+  return n > 1;
+}
+
+const res = console.log([...Array(101)].reduce((a, _, i) => a.concat(isSimple(i) ? `Делители этого числа ${i}: 1 и ${i}` : []) , []).join('\n')); 
