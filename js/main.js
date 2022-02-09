a693328999dfe7cdd1793dd633a96291343692a6
VadimKakoshkin <78777315+VadimKakoshkin@users.noreply.github.com>
2022-02-09 10:40:28 +0300

    Completed hard07

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-//Переменные
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+'use strict';
 
-//Логгеры
-alert("Hope has descended!");
-console.log("Console testing — Complete! =)");
+//Переменные
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+const day = new Date();
+let numWeekDay = day.getDay();
+if (numWeekDay === 0) { numWeekDay = 6; } else { numWeekDay--; }
+
+//Функции
+week.forEach((day, i) => {
+    let str = day;
+    if (i === numWeekDay) {
+        str = `<b>${day}</b>`;
+    } else {
+        str = `${day}`;
+    }
+    if (i === 5 || i === 6) { str = `<i>${str}</i>`; }
+    document.body.insertAdjacentHTML('beforeend', `<div>${str}</div>`);
+});
